eb6fdd5841f166a35cb3235d58dc05cc2d1c791d
Pedro Janeiro <budgerous@gmail.com>
2017-05-26 02:43:22 +0100

    Server and clients generating key pairs

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,114 +1,125 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.security.*;
 
-import java.net.*;
-import java.io.*;
 
-
-public class ChatClient implements Runnable
-{  
-    private Socket socket              = null;
-    private Thread thread              = null;
-    private DataInputStream  console   = null;
+public class ChatClient implements Runnable {
+    private Socket socket = null;
+    private Thread thread = null;
+    private DataInputStream console = null;
     private DataOutputStream streamOut = null;
-    private ChatClientThread client    = null;
+    private ChatClientThread client = null;
+    private PrivateKey privateKey = null;
+    private PublicKey publicKey = null;
 
-    public ChatClient(String serverName, int serverPort)
-    {  
+    public ChatClient(String serverName, int serverPort) {
         System.out.println("Establishing connection to server...");
-        
-        try
-        {
+
+        try {
             // Establishes connection with server (name and port)
             socket = new Socket(serverName, serverPort);
             System.out.println("Connected to server: " + socket);
-            start();
-        }
-        
-        catch(UnknownHostException uhe)
-        {  
+            setStreams();
+            if (handShake()) {
+                start();
+            } else {
+                System.out.println("Terminating.");
+                socket.close();
+            }
+        } catch (UnknownHostException uhe) {
             // Host unkwnown
-            System.out.println("Error establishing connection - host unknown: " + uhe.getMessage()); 
-        }
-      
-        catch(IOException ioexception)
-        {  
+            System.out.println("Error establishing connection - host unknown: " + uhe.getMessage());
+        } catch (IOException ioexception) {
             // Other error establishing connection
-            System.out.println("Error establishing connection - unexpected exception: " + ioexception.getMessage()); 
+            System.out.println("Error establishing connection - unexpected exception: " + ioexception.getMessage());
         }
-        
-   }
-    
-   public void run()
-   {  
-       while (thread != null)
-       {  
-           try
-           {  
-               // Sends message from console to server
-               streamOut.writeUTF(console.readLine());
-               streamOut.flush();
-           }
-         
-           catch(IOException ioexception)
-           {  
-               System.out.println("Error sending string to server: " + ioexception.getMessage());
-               stop();
-           }
-       }
+
     }
-    
-    
-    public void handle(String msg)
-    {  
+
+    public void run() {
+        while (thread != null) {
+            try {
+                // Sends message from console to server
+                streamOut.writeUTF(console.readLine());
+                streamOut.flush();
+            } catch (IOException ioexception) {
+                System.out.println("Error sending string to server: " + ioexception.getMessage());
+                stop();
+            }
+        }
+    }
+
+
+    public void handle(String msg) {
         // Receives message from server
-        if (msg.equals(".quit"))
-        {  
+        if (msg.equals(".quit")) {
             // Leaving, quit command
             System.out.println("Exiting...Please press RETURN to exit ...");
             stop();
-        }
-        else
+        } else
             // else, writes message received from server to console
             System.out.println(msg);
     }
-    
-    // Inits new client thread
-    public void start() throws IOException
-    {  
-        console   = new DataInputStream(System.in);
+
+    private void setStreams() throws IOException {
+        // Set streams from console and to socket
+        console = new DataInputStream(System.in);
         streamOut = new DataOutputStream(socket.getOutputStream());
-        if (thread == null)
-        {  
+    }
+
+    private boolean handShake() throws IOException {
+        DataInputStream streamIn = new DataInputStream(socket.getInputStream());
+        // Generate key pair
+        try {
+            System.out.println("Generating key pair.");
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+            SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+            keyGen.initialize(2048, random);
+
+            KeyPair pair = keyGen.generateKeyPair();
+            privateKey = pair.getPrivate();
+            publicKey = pair.getPublic();
+            System.out.println("Key pair generated.");
+        } catch (Exception e) {
+            System.out.println("Error generating key pair.");
+            return false;
+        }
+
+        System.out.println("Handshake completed.");
+        return true;
+    }
+
+    // Inits new client thread
+    public void start() throws IOException {
+        if (thread == null) {
             client = new ChatClientThread(this, socket);
-            thread = new Thread(this);                   
+            thread = new Thread(this);
             thread.start();
         }
     }
-    
+
     // Stops client thread
-    public void stop()
-    {  
-        if (thread != null)
-        {  
-            thread.stop();  
+    public void stop() {
+        if (thread != null) {
+            thread.stop();
             thread = null;
         }
-        try
-        {  
-            if (console   != null)  console.close();
-            if (streamOut != null)  streamOut.close();
-            if (socket    != null)  socket.close();
+        try {
+            if (console != null) console.close();
+            if (streamOut != null) streamOut.close();
+            if (socket != null) socket.close();
+        } catch (IOException ioe) {
+            System.out.println("Error closing thread...");
         }
-      
-        catch(IOException ioe)
-        {  
-            System.out.println("Error closing thread..."); }
-            client.close();  
-            client.stop();
-        }
-   
-    
-    public static void main(String args[])
-    {  
+        client.close();
+        client.stop();
+    }
+
+
+    public static void main(String args[]) {
         ChatClient client = null;
         if (args.length != 2)
             // Displays correct usage syntax on stdout
@@ -117,58 +128,43 @@ public class ChatClient implements Runnable
             // Calls new client
             client = new ChatClient(args[0], Integer.parseInt(args[1]));
     }
-    
+
 }
 
-class ChatClientThread extends Thread
-{  
-    private Socket           socket   = null;
-    private ChatClient       client   = null;
-    private DataInputStream  streamIn = null;
+class ChatClientThread extends Thread {
+    private Socket socket = null;
+    private ChatClient client = null;
+    private DataInputStream streamIn = null;
 
-    public ChatClientThread(ChatClient _client, Socket _socket)
-    {  
-        client   = _client;
-        socket   = _socket;
-        open();  
+    public ChatClientThread(ChatClient _client, Socket _socket) {
+        client = _client;
+        socket = _socket;
+        open();
         start();
     }
-   
-    public void open()
-    {  
-        try
-        {  
-            streamIn  = new DataInputStream(socket.getInputStream());
-        }
-        catch(IOException ioe)
-        {  
+
+    public void open() {
+        try {
+            streamIn = new DataInputStream(socket.getInputStream());
+        } catch (IOException ioe) {
             System.out.println("Error getting input stream: " + ioe);
             client.stop();
         }
     }
-    
-    public void close()
-    {  
-        try
-        {  
+
+    public void close() {
+        try {
             if (streamIn != null) streamIn.close();
-        }
-      
-        catch(IOException ioe)
-        {  
+        } catch (IOException ioe) {
             System.out.println("Error closing input stream: " + ioe);
         }
     }
-    
-    public void run()
-    {  
-        while (true)
-        {   try
-            {  
+
+    public void run() {
+        while (true) {
+            try {
                 client.handle(streamIn.readUTF());
-            }
-            catch(IOException ioe)
-            {  
+            } catch (IOException ioe) {
                 System.out.println("Listening error: " + ioe.getMessage());
                 client.stop();
             }
--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,241 +1,223 @@
-
-import java.net.*;
 import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.security.*;
 
 
-public class ChatServer implements Runnable
-{  
-	private ChatServerThread clients[] = new ChatServerThread[20];
-	private ServerSocket server_socket = null;
-	private Thread thread = null;
-	private int clientCount = 0;
+public class ChatServer implements Runnable {
+    private ChatServerThread clients[] = new ChatServerThread[20];
+    private ServerSocket server_socket = null;
+    private Thread thread = null;
+    private int clientCount = 0;
 
-	public ChatServer(int port)
-    	{  
-		try
-      		{  
-            		// Binds to port and starts server
-			System.out.println("Binding to port " + port);
-            		server_socket = new ServerSocket(port);  
-            		System.out.println("Server started: " + server_socket);
-            		start();
-        	}
-      		catch(IOException ioexception)
-      		{  
-            		// Error binding to port
-            		System.out.println("Binding error (port=" + port + "): " + ioexception.getMessage());
-        	}
-    	}
-    
-    	public void run()
-    	{  
-        	while (thread != null)
-        	{  
-            		try
-            		{  
-                		// Adds new thread for new client
-                		System.out.println("Waiting for a client ..."); 
-                		addThread(server_socket.accept()); 
-            		}
-            		catch(IOException ioexception)
-            		{
-                		System.out.println("Accept error: " + ioexception); stop();
-            		}
-        	}
-    	}
-    
-   	public void start()
-    	{  
-        	if (thread == null)
-        	{  
-            		// Starts new thread for client
-            		thread = new Thread(this); 
-            		thread.start();
-        	}
-    	}
-    
-    	public void stop()
-    	{  
-        	if (thread != null)
-        	{
-            		// Stops running thread for client
-            		thread.stop(); 
-            		thread = null;
-        	}
-    	}
-   
-    	private int findClient(int ID)
-    	{  
-        	// Returns client from id
-        	for (int i = 0; i < clientCount; i++)
-            		if (clients[i].getID() == ID)
-                		return i;
-        	return -1;
-    	}
-    
-    	public synchronized void handle(int ID, String input)
-    	{  
-        	if (input.equals(".quit"))
-            	{  
-                	int leaving_id = findClient(ID);
-                	// Client exits
-                	clients[leaving_id].send(".quit");
-                	// Notify remaing users
-                	for (int i = 0; i < clientCount; i++)
-                    		if (i!=leaving_id)
-                        		clients[i].send("Client " +ID + " exits..");
-                	remove(ID);
-            	}
-        	else
-            		// Brodcast message for every other client online
-            		for (int i = 0; i < clientCount; i++)
-                		clients[i].send(ID + ": " + input);   
-    	}
-    
-    	public synchronized void remove(int ID)
-    	{  
-        	int pos = findClient(ID);
-      
-       	 	if (pos >= 0)
-        	{  
-            		// Removes thread for exiting client
-            		ChatServerThread toTerminate = clients[pos];
-            		System.out.println("Removing client thread " + ID + " at " + pos);
-            		if (pos < clientCount-1)
-                		for (int i = pos+1; i < clientCount; i++)
-                    			clients[i-1] = clients[i];
-            		clientCount--;
-         
-            		try
-            		{  
-                		toTerminate.close(); 
-            		}
-         
-            		catch(IOException ioe)
-            		{  
-                		System.out.println("Error closing thread: " + ioe); 
-            		}
-         
-            		toTerminate.stop(); 
-        	}
-    	}
-    
-    	private void addThread(Socket socket)
-    	{  
-    	    	if (clientCount < clients.length)
-        	{  
-            		// Adds thread for new accepted client
-            		System.out.println("Client accepted: " + socket);
-            		clients[clientCount] = new ChatServerThread(this, socket);
-         
-           		try
-            		{  
-                		clients[clientCount].open(); 
-                		clients[clientCount].start();  
-                		clientCount++; 
-            		}
-            		catch(IOException ioe)
-            		{  
-               			System.out.println("Error opening thread: " + ioe); 
-            		}
-       	 	}
-        	else
-            		System.out.println("Client refused: maximum " + clients.length + " reached.");
-    	}
-    
-    
-	public static void main(String args[])
-   	{  
-        	ChatServer server = null;
-        
-        	if (args.length != 1)
-            		// Displays correct usage for server
-            		System.out.println("Usage: java ChatServer port");
-        	else
-            		// Calls new server
-            		server = new ChatServer(Integer.parseInt(args[0]));
-    	}
+    public ChatServer(int port) {
+        try {
+            // Binds to port and starts server
+            System.out.println("Binding to port " + port);
+            server_socket = new ServerSocket(port);
+            System.out.println("Server started: " + server_socket);
+            start();
+        } catch (IOException ioexception) {
+            // Error binding to port
+            System.out.println("Binding error (port=" + port + "): " + ioexception.getMessage());
+        }
+    }
+
+    public void run() {
+        while (thread != null) {
+            try {
+                // Adds new thread for new client
+                System.out.println("Waiting for a client ...");
+                addThread(server_socket.accept());
+            } catch (IOException ioexception) {
+                System.out.println("Accept error: " + ioexception);
+                stop();
+            }
+        }
+    }
+
+    public void start() {
+        if (thread == null) {
+            // Starts new thread for client
+            thread = new Thread(this);
+            thread.start();
+        }
+    }
+
+    public void stop() {
+        if (thread != null) {
+            // Stops running thread for client
+            thread.stop();
+            thread = null;
+        }
+    }
+
+    private int findClient(int ID) {
+        // Returns client from id
+        for (int i = 0; i < clientCount; i++)
+            if (clients[i].getID() == ID)
+                return i;
+        return -1;
+    }
+
+    public synchronized void handle(int ID, String input) {
+        if (input.equals(".quit")) {
+            int leaving_id = findClient(ID);
+            // Client exits
+            clients[leaving_id].send(".quit");
+            // Notify remaing users
+            for (int i = 0; i < clientCount; i++)
+                if (i != leaving_id)
+                    clients[i].send("Client " + ID + " exits..");
+            remove(ID);
+        } else
+            // Brodcast message for every other client online
+            for (int i = 0; i < clientCount; i++)
+                clients[i].send(ID + ": " + input);
+    }
+
+    public synchronized void remove(int ID) {
+        int pos = findClient(ID);
+
+        if (pos >= 0) {
+            // Removes thread for exiting client
+            ChatServerThread toTerminate = clients[pos];
+            System.out.println("Removing client thread " + ID + " at " + pos);
+            if (pos < clientCount - 1)
+                for (int i = pos + 1; i < clientCount; i++)
+                    clients[i - 1] = clients[i];
+            clientCount--;
+
+            try {
+                toTerminate.close();
+            } catch (IOException ioe) {
+                System.out.println("Error closing thread: " + ioe);
+            }
+
+            toTerminate.stop();
+        }
+    }
+
+    private void addThread(Socket socket) {
+        if (clientCount < clients.length) {
+            // Adds thread for new accepted client
+            System.out.println("Client accepted: " + socket);
+            clients[clientCount] = new ChatServerThread(this, socket);
+
+            try {
+                clients[clientCount].open();
+                if (clients[clientCount].handShake()) {
+                    clients[clientCount].start();
+                    clientCount++;
+                } else {
+                    clients[clientCount].send(".quit");
+                    remove(clients[clientCount].getID());
+                }
+            } catch (IOException ioe) {
+                System.out.println("Error opening thread: " + ioe);
+            }
+        } else
+            System.out.println("Client refused: maximum " + clients.length + " reached.");
+    }
+
+
+    public static void main(String args[]) {
+        ChatServer server = null;
+
+        if (args.length != 1)
+            // Displays correct usage for server
+            System.out.println("Usage: java ChatServer port");
+        else
+            // Calls new server
+            server = new ChatServer(Integer.parseInt(args[0]));
+    }
 
 }
 
-class ChatServerThread extends Thread
-{  
-    private ChatServer       server    = null;
-    private Socket           socket    = null;
-    private int              ID        = -1;
-    private DataInputStream  streamIn  =  null;
+class ChatServerThread extends Thread {
+    private ChatServer server = null;
+    private Socket socket = null;
+    private int ID = -1;
+    private DataInputStream streamIn = null;
     private DataOutputStream streamOut = null;
+    private PrivateKey privateKey = null;
+    private PublicKey publicKey = null;
 
-   
-    public ChatServerThread(ChatServer _server, Socket _socket)
-    {  
+
+    public ChatServerThread(ChatServer _server, Socket _socket) {
         super();
         server = _server;
         socket = _socket;
-        ID     = socket.getPort();
+        ID = socket.getPort();
     }
-    
+
     // Sends message to client
-    public void send(String msg)
-    {   
-        try
-        {  
+    public void send(String msg) {
+        try {
             streamOut.writeUTF(msg);
             streamOut.flush();
-        }
-       
-        catch(IOException ioexception)
-        {  
+        } catch (IOException ioexception) {
             System.out.println(ID + " ERROR sending message: " + ioexception.getMessage());
             server.remove(ID);
             stop();
         }
     }
-    
+
     // Gets id for client
-    public int getID()
-    {  
+    public int getID() {
         return ID;
     }
-   
+
     // Runs thread
-    public void run()
-    {  
+    public void run() {
         System.out.println("Server Thread " + ID + " running.");
-      
-        while (true)
-        {  
-            try
-            {  
+
+        while (true) {
+            try {
                 server.handle(ID, streamIn.readUTF());
-            }
-         
-            catch(IOException ioe)
-            {  
+            } catch (IOException ioe) {
                 System.out.println(ID + " ERROR reading: " + ioe.getMessage());
                 server.remove(ID);
                 stop();
             }
         }
     }
-    
-    
+
+
     // Opens thread
-    public void open() throws IOException
-    {  
-        streamIn = new DataInputStream(new 
-                        BufferedInputStream(socket.getInputStream()));
+    public void open() throws IOException {
+        streamIn = new DataInputStream(new
+                BufferedInputStream(socket.getInputStream()));
         streamOut = new DataOutputStream(new
-                        BufferedOutputStream(socket.getOutputStream()));
+                BufferedOutputStream(socket.getOutputStream()));
     }
-    
+
     // Closes thread
-    public void close() throws IOException
-    {  
-        if (socket != null)    socket.close();
-        if (streamIn != null)  streamIn.close();
+    public void close() throws IOException {
+        if (socket != null) socket.close();
+        if (streamIn != null) streamIn.close();
         if (streamOut != null) streamOut.close();
     }
-    
+
+    public boolean handShake() {
+        // Generate key pair
+        try {
+            System.out.println("Generating key pair.");
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+            SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+            keyGen.initialize(2048, random);
+
+            KeyPair pair = keyGen.generateKeyPair();
+            privateKey = pair.getPrivate();
+            publicKey = pair.getPublic();
+            System.out.println("Key pair generated.");
+        } catch (Exception e) {
+            System.out.println("Error generating key pair.");
+            return false;
+        }
+
+        System.out.println("Handshake completed.");
+        return true;
+    }
 }
 
